b6fed5549c54d6451387fee47b0a4b26ea3810de
WelitonSalgado <weliton.salgado@gmail.com>
2025-11-14 15:26:10 -0300

    finish `/api/v1/status` endpoint.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "HTTP é muito massa." });
+  //const result = await database.query("SELECT 1 + 1 as sum;");
+  //console.log(result.rows);
+
+  const updatedAt = new Date().toISOString();
+  
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB
+  const dbOpenedConnetions = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
+  const dbOpenedConnetionsValue = dbOpenedConnetions.rows[0].count;
+
+  response.status(200).json({ 
+    updated_at: updatedAt, 
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connetions: dbOpenedConnetionsValue,
+      }
+    }
+   });
 }
 
 export default status;
