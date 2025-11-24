b750b3c2a1a15a1ef6b4ec0daa5d368a25e015f5
WelitonSalgado <weliton.salgado@gmail.com>
2025-11-24 14:32:18 -0300

    test: rename all descriptions to use new pattern

--- tests/integrations/api/v1/migrations/post.test.js
+++ tests/integrations/api/v1/migrations/post.test.js
@@ -6,22 +6,35 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1Body = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the firs time", async () => {
+        const response1Body = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1Body.status).toBe(201);
+
+        const response1BodyBody = await response1Body.json();
+        expect(Array.isArray(response1BodyBody)).toBe(true);
+        expect(response1BodyBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const response2Body = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2Body.status).toBe(200);
+
+        const response2BodyBody = await response2Body.json();
+        expect(Array.isArray(response2BodyBody)).toBe(true);
+        expect(response2BodyBody.length).toBe(0);
+      });
+    });
   });
-  expect(response1Body.status).toBe(201);
-
-  const response1BodyBody = await response1Body.json();
-  expect(Array.isArray(response1BodyBody)).toBe(true);
-  expect(response1BodyBody.length).toBeGreaterThan(0);
-
-  const response2Body = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2Body.status).toBe(200);
-
-  const response2BodyBody = await response2Body.json();
-  expect(Array.isArray(response2BodyBody)).toBe(true);
-  expect(response2BodyBody.length).toBe(0);
 });
